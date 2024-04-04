6fdc20837375e3a9a57c1818d3e30d5843ba1b22
Ángeles Rondón <angelesemilianaa@gmail.com>
2024-04-04 11:07:50 -0500

    Update favicon and logo
    * Delete vue default favicon and logo assets.
    * Add E-Cheve logo and favicon.
    * Replace heading with E-Cheve logo to make it look better.

--- e-cheve-front/src/components/HeaderCheve.vue
+++ e-cheve-front/src/components/HeaderCheve.vue
@@ -1,5 +1,7 @@
 <template>
-  <header class="relative top-0 right-0 p-5">
-    <h1 class="text-4xl font-serif text-red-950">E-cheve</h1>
+  <header class="h-32 relative top-0 right-0 p-5">
+    <figure class="h-full">
+      <img src="../assets/e-cheve-icon.png" alt="E cheve logo" class="h-full" />
+    </figure>
   </header>
 </template>
