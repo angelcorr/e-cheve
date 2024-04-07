584af52fc5561ad77bbf020a2cdcfc81fedd70a0
Ángeles Rondón <angelesemilianaa@gmail.com>
2024-04-06 21:17:56 -0500

    Run prettier to format files

--- e-cheve-front/tailwind.config.js
+++ e-cheve-front/tailwind.config.js
@@ -1,15 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './index.html', './src/**/*.{vue,js,ts,jsx,tsx}'
-  ],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
-        'linen': '#ffeddf',
-      },
+        linen: '#ffeddf'
+      }
     }
   },
-  plugins: [],
+  plugins: []
 }
-
